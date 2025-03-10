5f06eb6016b562f5578f3d2db5557014e458f3d3
Amit Gupta <110421665+ameitg@users.noreply.github.com>
2025-03-10 13:24:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> `whoami`/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> `whoami`/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
