2112770f82402d005574723e871d4dab10b07dcf
Amit Gupta <110421665+ameitg@users.noreply.github.com>
2025-03-10 13:31:51 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install npm
 npm install -g npm #upgrade NPM
 brew install openjdk #install JDK for android studio
+echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 brew install android-studio #install Android studio
 https://apps.apple.com/us/app/xcode/id497799835?mt=12 #Install xcode
 
